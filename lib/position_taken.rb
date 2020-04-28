1f193b4b094ddc81c586518747daa72c0ec06142
Joann Pan <joannlpan@gmail.com>
2020-04-28 17:42:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,9 @@
 def position_taken? (board, index)
+<<<<<<< HEAD
   if board[index] == " " || board[index] == "" || board[index] == nil
+=======
+  if board[index] == " " || ""
+>>>>>>> c10c89007a72ae24d8e25da0cfc741ab0c6e96f4
   return false
   else board[index] == "X" || "O"
   return true
